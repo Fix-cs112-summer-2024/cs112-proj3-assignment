d663636e369f1757931f6c5abbc35849ddb3fcee
Victor Norman <vtn2@calvin.edu>
2022-07-08 12:28:17 -0400

    update

---
+++ HighScores.h
@@ -0,0 +1,5 @@
+#ifndef _HIGH_SCORES_H_
+#define _HIGH_SCORES_H_
+
+
+#endif
--- tests.cpp
+++ tests.cpp
@@ -1,10 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "Student.h"
 
-TEST_CASE("student getters") {
-    Student st;
-    // SECTION("getMajor") {
-    //     REQUIRE(st.getMajor() == "Undecided");
-    // }
+TEST_CASE("") {
+
 }
